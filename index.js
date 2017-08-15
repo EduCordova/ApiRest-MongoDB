e9e87dfd41cab680e5f03c09fd5a185487ee1256
Edu <kde1690@gmail.com>
2017-08-14 23:26:49 -0500

    Update index.js
    change url

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = require('./app')
 const port = process.env.PORT || 3001
 
 
-mongoose.connect('mongodb://localhost:27017/shop',{ useMongoClient: true }, (err, res) => {
+mongoose.connect('mongodb://adminapi:123456@ds155841.mlab.com:55841/shop',{ useMongoClient: true }, (err, res) => {
     if (err) {
         return console.log('error al conectarse A la DB')
     } else {
